70ed9d865f374f1db4b2ccfe8cc2994ae1307600
agum.ruswandi10@gmail.com <agum.ruswandi10@gmail.com>
2025-08-19 17:48:34 +0700

    update file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
+    pageLoadTimeout: 120000, 
+    defaultCommandTimeout: 10000,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
